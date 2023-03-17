cde42aae707885ee2283417f4a2242de945fce8a
fiorella frini <fiorella_frini@hotmail.com>
2023-03-17 18:44:27 -0300

    dogs11

--- client/src/components/4LandingPage/LandingPage.jsx
+++ client/src/components/4LandingPage/LandingPage.jsx
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom";
-// import video from "../13Extras/2.mp4"
+import video1 from "../13Extras/video1.mp4"
 import styles from "../4LandingPage/LandingPage.module.css"
 
 const LandingPage = () => {
     return(
-        <div className={styles.body}>
-                {/* <div> */}
-                    <h1>Welcome to Dogs APP</h1>
+        <div className={styles.bodyLanding}>
+                    <div className={styles.title1}>
+                        <h1>Welcome</h1>
+                    </div>
 
-                    <button>
-                    <Link to="/home">Home</Link>
-                    </button>
+                    <div className={styles.title2}>
+                        to the Dogs Api!
+                    </div>
+                    
+                    <div className={styles.land}>
+                        <Link  to="/home"><button>HOME</button></Link>
+                        <Link to="/about"><button>ABOUT</button></Link>
+                    </div>
 
-                    <button>
-                    <Link to="/about">About</Link>
-                    </button>
-                {/* </div> */}
-
-            {/* <video autoPlay muted loop className={styles.video}>
-             <source src={video} type="video/mp4"></source>
-            </video> */}
+                    <video autoPlay muted loop className={styles.video}>
+                        <source src={video1} type="video/mp4"></source>
+                    </video>
        
         </div>
     )
