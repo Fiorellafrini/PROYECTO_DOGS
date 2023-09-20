90aae32a34f95b8a21f212ca8fb0b4af66ff6281
fiorella frini <fiorella_frini@hotmail.com>
2023-09-20 11:23:57 -0300

    deployback

--- api/index.js
+++ api/index.js
@@ -20,14 +20,14 @@
 const server = require('./src/app.js');
 const { getAllTemp } = require('./src/Controllers/temperamentControllers.js');
 const { conn } = require('./src/db.js');
-
+const port = process.env.PORT || 3001
 
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, async() => {
+  server.listen(port, async() => {
   await getAllTemp() 
 
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,20 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST
+  DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+//   logging: false, 
+//   native: false, 
+// });
+
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  logging: false, 
+  native: false, 
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
